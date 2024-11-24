3b6f148eb75b93dd49a04ddf4d17b905318feaa5
Fernando Alves Carreira <fe.alves_carreira@hotmail.com>
2024-11-24 20:22:26 -0300

    Criação da página principal e trabalhando com arquivos estáticos

--- app_recipes/urls.py
+++ app_recipes/urls.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
-from app_recipes.views import contato, fotos, home, sobre
+from app_recipes.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato),
-    path('fotos/', fotos)
+    path('', home)
 ]
--- projeto_curso/urls.py
+++ projeto_curso/urls.py
@@ -18,8 +18,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from app_recipes.views import contato, home, sobre
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app_recipes.urls'))
